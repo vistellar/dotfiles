6659b5ed809a21c225b32cd52470374ef305e4f7
weixing-org <weixing-org@gmail.com>
2016-07-12 08:28:31 +0800

    Update Emacs init config file

--- .config/emacs/init-local.el
+++ .config/emacs/init-local.el
@@ -1,10 +1,14 @@
+;;; init-local.el --- Local config
 ;;;
+;;; Commentary:
 ;;;
+;;; Code:
 
-;; org
+;;;
+(setq org-agenda-files
+      (quote ("~/todo.org" "~/note.org")))
 
-(setq org-agenda-files (quote ("~/todo.org" "~/note.org")))
-
+;;;
 (setq org-capture-templates
       `(("t" "Todo" entry (file+headline "~/todo.org" "TODO")
          "* NEXT %?\n%U\n" :clock-resume t)
@@ -12,4 +16,20 @@
          "z* %? :NOTE:\n%U\n%a\n" :clock-resume t)
         ))
 
+
+;;;
+(defun open-init-local-file ()
+  "Open the init file in config path."
+  (interactive)
+  (find-file "~/.config/emacs/init-local.el"))
+(global-set-key (kbd "<f2>") 'open-init-local-file)
+
+(linum-mode t)
+(global-hl-line-mode t)
+
+(setq-default cursor-type 'bar)
+(setq make-backup-files nil)
+(set-frame-font "DejaVu Sans Mono-16" nil t)
+
 (provide 'init-local)
+;;; init-local.el ends here
