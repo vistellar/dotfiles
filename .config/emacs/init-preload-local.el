3a95aa28ab4b6fef1c6d1a7ed0351404e6a75ab7
weixing-org <weixing-org@gmail.com>
2017-05-07 12:13:20 +0800

    Delete some files

--- .config/emacs/init-preload-local.el
+++
@@ -1,7 +0,0 @@
-;;; init-preload-local --- Init proload config
-;;;
-
-(add-to-list 'package-archives `("melpa-cn" . ("http://elpa.zilongshanren.com/melpa/")))
-
-(provide 'init-preload-local)
-;;; init-preload-local.el ends here
